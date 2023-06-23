28c4b12b62915968d33cda73fadb1176cc697d1f
FurkanAydar <furkanaydar97@gmail.com>
2023-06-23 20:21:59 +0300

    DailyTemperatures

---
+++ src/grind75/Week3/DailyTemperatures.java
@@ -0,0 +1,36 @@
+package grind75.Week3;
+
+import java.util.Stack;
+
+public class DailyTemperatures {
+
+    public static int[] dailyTemperatures(int[] temperatures) {
+        int[] res = new int[temperatures.length];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i=0 ;i<temperatures.length; i++) {
+            if (st.isEmpty()) {
+                st.add(i);
+                continue;
+            }
+
+            if (temperatures[st.peek()] < temperatures[i]) {
+                while (!st.isEmpty() && temperatures[st.peek()] < temperatures[i]) {
+                    int popped = st.pop();
+                    res[popped] = i-popped;
+                }
+            }
+
+            st.add(i);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] in = new int[] {73,74,75,71,69,72,76,73};
+        int[] in2 = new int[] {30, 40, 50, 60};
+
+        dailyTemperatures(in);
+    }
+}
