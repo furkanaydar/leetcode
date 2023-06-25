96a3a8b7938563b4c629f6a3d30dac335b3ed9f0
FurkanAydar <furkanaydar97@gmail.com>
2023-06-26 02:42:28 +0300

    GroupAnagrams

---
+++ src/grind75/Week3/GroupAnagrams.java
@@ -0,0 +1,53 @@
+package grind75.Week3;
+
+import java.util.*;
+
+public class GroupAnagrams {
+
+    class Entry implements Comparable<Entry> {
+        int index;
+        String sortedString;
+
+        public Entry(int index, String sortedString) {
+            this.index = index;
+            this.sortedString = sortedString;
+        }
+
+        @Override
+        public int compareTo(Entry o) {
+            return this.sortedString.compareTo(o.sortedString) < 0 ? -1 : 1;
+        }
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<Entry> entries = new ArrayList<>();
+
+        for (int i=0; i<strs.length; i++) {
+            char[] arr = strs[i].toCharArray();
+            Arrays.sort(arr);
+            entries.add(new Entry(i, new String(arr)));
+        }
+
+        Collections.sort(entries);
+        String it = entries.get(0).sortedString;
+        List<List<String>> res = new ArrayList<>();
+        List<String> cur = new ArrayList<>();
+        for (Entry entry : entries) {
+            if (!Objects.equals(entry.sortedString, it)) {
+                it = entry.sortedString;
+                res.add(cur);
+                cur = new ArrayList<>();
+            }
+
+            cur.add(strs[entry.index]);
+        }
+
+        res.add(cur);
+        return res;
+    }
+
+    public static void main(String[] args) {
+        GroupAnagrams c = new GroupAnagrams();
+        c.groupAnagrams(new String[] {"eat","tea","tan","ate","nat","bat"});
+    }
+}
