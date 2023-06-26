a9ba531e29cc04918cf63fd09bbe25ec9e003db9
FurkanAydar <furkanaydar97@gmail.com>
2023-06-26 17:46:58 +0300

    RemoveNthNodeFromEnd

---
+++ src/grind75/Week3/RemoveNthNodeFromEnd.java
@@ -0,0 +1,28 @@
+package grind75.Week3;
+
+import common.ListNode;
+
+public class RemoveNthNodeFromEnd {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int c = 0;
+        ListNode tmp = head, res = head;
+        ListNode prev = null;
+        while (tmp != null) {
+            if (c>=n) {
+                prev = head;
+                head = head.next;
+            }
+
+            tmp = tmp.next;
+            c++;
+        }
+
+        if (prev == null) {
+            return res.next;
+        }
+
+        prev.next = prev.next.next;
+        return res;
+    }
+}
