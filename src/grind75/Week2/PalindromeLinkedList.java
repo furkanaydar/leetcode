8bc1d9d058da8eee6802342807e184171c7b7012
FurkanAydar <furkanaydar97@gmail.com>
2023-06-27 15:26:24 +0300

    PalindromeLinkedList

---
+++ src/grind75/Week2/PalindromeLinkedList.java
@@ -0,0 +1,51 @@
+package grind75.Week2;
+
+import common.ListNode;
+
+public class PalindromeLinkedList {
+
+    public ListNode reverse(ListNode prev, ListNode head) {
+        while (head != null) {
+            ListNode tmp = head.next;
+            head.next = prev;
+            prev = head;
+            head = tmp;
+        }
+
+        return prev;
+    }
+
+    public ListNode getMiddleNode(ListNode head) {
+        ListNode slow = head;
+
+        while (head != null) {
+            head = head.next;
+
+            if (head != null) {
+                head = head.next;
+            }
+
+            if (head != null) {
+                slow = slow.next;
+            }
+        }
+
+        return slow;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode middle = getMiddleNode(head);
+        ListNode endNode = reverse(middle, middle.next);
+        middle.next = null;
+        while (head != null && endNode != null) {
+            if (head.val != endNode.val) {
+                return false;
+            }
+
+            head = head.next;
+            endNode = endNode.next;
+        }
+
+        return true;
+    }
+}
