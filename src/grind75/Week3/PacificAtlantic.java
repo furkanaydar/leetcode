c460d6acd73fee6071a6d8953555c6780e410820
FurkanAydar <furkanaydar97@gmail.com>
2023-07-01 03:42:44 +0300

    PacificAtlantic & ValidSudoku

---
+++ src/grind75/Week3/PacificAtlantic.java
@@ -0,0 +1,57 @@
+package grind75.Week3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PacificAtlantic {
+    int[][] d = new int[][] {
+            {0, 1},
+            {1, 0},
+            {0, -1},
+            {-1, 0}
+    };
+
+    void dfs(int[][] heights, int x, int y, boolean[][] h) {
+        if (h[x][y]) {
+            return;
+        }
+
+        h[x][y] = true;
+        for (int[] nd: d) {
+            int nx = x+nd[0], ny = y+nd[1];
+            if (nx<0 || nx>= heights.length || ny<0 || ny>=heights[0].length) {
+                continue;
+            }
+
+            if (!h[nx][ny] && heights[x][y] <= heights[nx][ny]) {
+                dfs(heights, nx, ny, h);
+            }
+        }
+    }
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        boolean[][] pacificVisited = new boolean[heights.length][heights[0].length];
+        boolean[][] atlanticVisited = new boolean[heights.length][heights[0].length];
+
+        for (int i=0; i<heights.length; i++) {
+            dfs(heights, i, 0, pacificVisited);
+            dfs(heights, i, heights[i].length-1, atlanticVisited);
+        }
+
+        for (int i=0; i<heights[0].length; i++) {
+            dfs(heights, 0, i, pacificVisited);
+            dfs(heights, heights.length-1, i, atlanticVisited);
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i=0; i<pacificVisited.length; i++) {
+            for (int j=0; j<pacificVisited[0].length; j++) {
+                if (pacificVisited[i][j] && atlanticVisited[i][j]) {
+                    res.add(List.of(i, j));
+                }
+            }
+        }
+
+        return res;
+    }
+}
---
+++ src/grind75/Week3/ValidSudoku.java
@@ -0,0 +1,23 @@
+package grind75.Week3;
+
+public class ValidSudoku {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] groups = new boolean[9][9];
+
+        for (int i=0; i<board.length; i++) {
+            for(int j=0; j<board[i].length; j++) {
+                if (Character.isDigit(board[i][j])) {
+                    int n = board[i][j]-'0';
+                    if (rows[i][n-1] || cols[j][n-1] || groups[(i/3)*3+j/3][n-1]) {
+                        return false;
+                    }
+                    rows[i][n-1] = cols[j][n-1] = groups[(i/3)*3+j/3][n-1] = true;
+                }
+            }
+        }
+
+        return true;
+    }
+}
