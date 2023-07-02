db4b4d3ba75fdf4475d71593ffa9b330d6e4faa3
Furkan Aydar <furkan.aydar@klarna.com>
2023-07-02 20:28:28 +0200

    A simple DoublyLinkedList implementation

---
+++ src/common/DoublyLinkedList.java
@@ -0,0 +1,201 @@
+package common;
+
+public class DoublyLinkedList<T> {
+
+    DoublyLinkedListNode<T> head;
+    DoublyLinkedListNode<T> tail;
+    int listLength=0;
+
+    public void insertToBeginning(T val) {
+        DoublyLinkedListNode<T> newNode = new DoublyLinkedListNode<>(val);
+        if (listLength > 0) {
+            newNode.next = head;
+            head.prev = newNode;
+        }
+
+        head = newNode;
+
+        if (listLength == 0) {
+            tail = head;
+        }
+
+        listLength++;
+    }
+
+    public void insertToEnd(T val) {
+        DoublyLinkedListNode<T> newNode = new DoublyLinkedListNode<>(val);
+        if (listLength > 0) {
+            tail.next = newNode;
+            newNode.prev = tail;
+        }
+
+        tail = newNode;
+
+        if (listLength == 0) {
+            head = tail;
+        }
+
+        listLength++;
+    }
+
+    public void insertAfterIndex(int index, T val) {
+        DoublyLinkedListNode<T> node = getNode(index);
+
+        if (node == null) {
+            throw new RuntimeException();
+        }
+
+        DoublyLinkedListNode<T> nextNode = node.next;
+        DoublyLinkedListNode<T> newNode = new DoublyLinkedListNode<>(val);
+        node.next = newNode;
+        newNode.prev = node;
+
+        if (nextNode != null) {
+            nextNode.prev = newNode;
+        }
+
+        newNode.next = nextNode;
+        listLength++;
+    }
+
+    public T get(int index) {
+        DoublyLinkedListNode<T> node = getNode(index);
+        return node == null ? null : node.val;
+    }
+
+    public T removeFromBeginning() {
+        if (head == null) {
+            throw new RuntimeException();
+        }
+
+        T removedVal = head.val;
+        head = head.next;
+        if (head != null) {
+            head.prev = null;
+        }
+
+        listLength--;
+
+        if (listLength == 1) {
+            tail = head;
+        }
+
+        if (listLength == 0) {
+            tail = null;
+        }
+
+        return removedVal;
+    }
+
+    public T removeFromEnd() {
+        if (tail == null) {
+            throw new RuntimeException();
+        }
+
+        T removedVal = tail.val;
+        tail = tail.prev;
+        if (tail != null) {
+            tail.next = null;
+        }
+
+        listLength--;
+
+        if (listLength == 1) {
+            head = tail;
+        }
+
+        if (listLength == 0) {
+            head = null;
+        }
+
+        return removedVal;
+    }
+
+    public T removeNthIndex(int index) {
+        DoublyLinkedListNode<T> node = getNode(index);
+
+        if (node == null) {
+            throw new RuntimeException();
+        }
+
+        if (index == 0) {
+            return removeFromBeginning();
+        }
+
+        if (index == listLength-1) {
+            return removeFromEnd();
+        }
+
+        DoublyLinkedListNode<T> nextNode = node.next;
+        nextNode.prev = node.prev;
+        node.prev.next = nextNode;
+        return node.val;
+    }
+
+    public int firstOccurrence(T val) {
+        DoublyLinkedListNode<T> tmp = head;
+        int index = -1;
+
+        while (tmp != null) {
+            index++;
+            if (tmp.val.equals(val)) {
+                return index;
+            }
+
+            tmp = tmp.next;
+        }
+
+        return -1;
+    }
+
+    public int length() {
+        return listLength;
+    }
+
+    private DoublyLinkedListNode<T> getNode(int index) {
+        if (index < 0 || index >= listLength) {
+            throw new RuntimeException();
+        }
+
+        DoublyLinkedListNode<T> tmp = head;
+        int curIndex = 0;
+
+        while (tmp != null && curIndex<index) {
+            curIndex++;
+            tmp = tmp.next;
+        }
+
+        return curIndex == index && tmp != null ? tmp : null;
+    }
+
+    class DoublyLinkedListNode<U> {
+        U val;
+        DoublyLinkedListNode<U> next;
+        DoublyLinkedListNode<U> prev;
+
+        public DoublyLinkedListNode(U val) {
+            this.val = val;
+            this.next = this.prev = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList<Integer> ll = new DoublyLinkedList<>();
+        ll.insertToEnd(3);
+        ll.removeFromBeginning();
+        ll.insertToBeginning(5);
+        ll.removeFromEnd();
+        ll.insertToBeginning(6);
+        ll.insertToEnd(4);
+        System.out.println(ll.get(0));
+        System.out.println(ll.get(1));
+        ll.removeFromEnd();
+        ll.removeFromEnd();
+        ll.insertToEnd(8);
+        ll.insertToBeginning(3);
+        ll.insertToEnd(10);
+        System.out.println(ll.get(1));
+        System.out.println(ll.removeNthIndex(1));
+        System.out.println(ll.get(1));
+    }
+}
